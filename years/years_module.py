8fb86c3c292ab7eeeb615858331d58375c74074b
Áron Kékes <kekes.aron@gmail.com>
2016-05-30 14:16:24 +0200

    Years_Finished

--- years/years_module.py
+++ years/years_module.py
@@ -1,11 +1,16 @@
-import datetime
+from datetime import date
 
 
 def years(age):
-    return
+    today = date.today()
+    oldage = today.year - age + 100
+    return oldage
 
 
 def main():
+    name = input('Please enter your name: ')
+    age = int(input('Please enter Your age: '))
+    print(years(age))
     return
 
 
