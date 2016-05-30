75418f0afdda49a533115656dbdf90c83c7450e5
Áron Kékes <kekes.aron@gmail.com>
2016-05-30 11:38:56 +0200

    Finished FizzBuzz

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,16 @@
 def fizzbuzz(number):
-    return
+    if number % 15 == 0:
+        number = "FizzBuzz"
+    elif number % 5 == 0:
+        number = "Buzz"
+    elif number % 3 == 0:
+        number = "Fizz"
+    return number
 
 
 def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
