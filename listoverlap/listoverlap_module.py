939ea2e1e56040f501c668b8513f959e32fd195d
Áron Kékes <kekes.aron@gmail.com>
2016-05-30 13:47:37 +0200

    Listoverlap_Finished

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,8 +1,18 @@
 def listoverlap(list1, list2):
-    return
+    common_elements = []
+    for i in list1:
+        for j in list2:
+            if i == j and j not in common_elements:
+                common_elements.append(j)
+
+    return common_elements
 
 
 def main():
+    list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    common_elements = listoverlap(list1, list2)
+    print(common_elements)
     return
 
 
