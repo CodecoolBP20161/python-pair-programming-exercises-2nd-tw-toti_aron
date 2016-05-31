f0506d286dce8749bd1fb86f591d1e5c4ea0df3c
Áron Kékes <kekes.aron@gmail.com>
2016-05-31 09:58:44 +0200

    palindrome ready

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,21 @@
-def palindrome(str):
-    return
+def palindrome(word):
+    result = ""
+    word = word.lower()
+    word = word.replace(" ", '')
+    if word == word[::-1]:
+        result = True
+    else:
+        result = False
+    return result
 
 
 def main():
+    word=input("Please enter a word and I will tell you that it's a palindrome or not: ")
+    status = palindrome(word)
+    if status == False:
+        print("This is not a palindrome")
+    else:
+        print("This is a palindrome")
     return
 
 
