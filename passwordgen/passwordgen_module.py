0580d435a665ae9c34807a2c51660b34b89d01d8
Áron Kékes <kekes.aron@gmail.com>
2016-06-01 10:20:41 +0200

    passwordgen ready

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,33 @@
+import random
+import string
+
+
 def passwordgen():
-    return
+    alphabet_upp = string.ascii_uppercase
+    alphabet_low = string.ascii_lowercase
+    num = str("0123456789")
+    sym = str("!@#$%^&*()?")
+    pass_length = 8
+    mypass = ''
+
+    for i in range(0, 2):
+        next_index = random.randrange(len(alphabet_low))
+        mypass = mypass + alphabet_upp[next_index]
+    for j in range(0, 2):
+        next_index = random.randrange(len(alphabet_upp))
+        mypass = mypass + alphabet_low[next_index]
+    for k in range(0, 2):
+        next_index = random.randrange(len(num))
+        mypass = mypass + num[next_index]
+    for k in range(0, 2):
+        next_index = random.randrange(len(sym))
+        mypass = mypass + sym[next_index]
+    return mypass
 
 
 def main():
+    passw = passwordgen()
+    print(passw)
     return
 
 
